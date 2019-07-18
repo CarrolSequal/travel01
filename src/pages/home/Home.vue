90b1b6ad28509bb6ae9e0d92e0c8fec21d2ef181
CarrolSequal <523791879@qq.com>
2019-07-18 23:34:15 +0800

    up0718night

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,18 +3,21 @@
     <!-- hola mundo -->
     <home-header></home-header>
     <home-swiper></home-swiper>
-    <div>hola</div>
+    <home-icons></home-icons>
+    <!-- <div>hola</div> -->
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
