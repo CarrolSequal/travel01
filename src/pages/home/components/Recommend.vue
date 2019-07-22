a0157e33351f974139144dc7f684e67d3f2d78af
CarrolSequal <GABRIELLE.WANG@OOCL.COM>
2019-07-22 11:31:41 +0800

    0722m index-recommand update

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">RECOMMEND</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item,index) of reommendList" :key="index">
+          <img class="item-img" :src='item.imgUrl' />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">Show Detials</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      reommendList: [{
+      id: '0001',
+      imgUrl: 'http://img1.qunarzz.com/sight/p0/1503/b4/b4834f16bc8f8091.water.jpg_200x200_be5f7395.jpg',
+      title: '珠海长隆海洋王国',
+      desc: '广东省珠海市横琴新区富祥湾'
+    },{
+      id: '0002',
+      imgUrl: 'http://img1.qunarzz.com/sight/p0/1503/b4/b4834f16bc8f8091.water.jpg_200x200_be5f7395.jpg',
+      title: '珠海长隆海洋王国',
+      desc: '广东省珠海市横琴新区富祥湾'
+    },{
+      id: '0003',
+      imgUrl: 'http://img1.qunarzz.com/sight/p0/1503/b4/b4834f16bc8f8091.water.jpg_200x200_be5f7395.jpg',
+      title: '珠海长隆海洋王国',
+      desc: '广东省珠海市横琴新区富祥湾'
+    }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~style/varibles.styl'
+  @import '~style/mixins.styl'
+  .title
+    margin-top .2rem
+    height: .8rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem 
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #cccccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding:0 .2rem
+        border-radius: .06rem
+        color #fff
+
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="title">WEEKEND TO GO</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item,index) of reommendList" :key="index">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src='item.imgUrl' />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      reommendList: [{
+      id: '0001',
+      imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+      title: '珠海长隆海洋王国',
+      desc: '广东省珠海市横琴新区富祥湾'
+    },{
+      id: '0002',
+      imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+      title: '珠海长隆海洋王国',
+      desc: '广东省珠海市横琴新区富祥湾'
+    },{
+      id: '0003',
+      imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+      title: '珠海长隆海洋王国',
+      desc: '广东省珠海市横琴新区富祥湾'
+    }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~style/varibles.styl'
+  @import '~style/mixins.styl'
+  .title
+    margin-top .2rem
+    height: .8rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9% 
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #cccccc
+      ellipsis()
+
+</style>
