736a62530851b78afded3770dca0d2fd00f70b86
CarrolSequal <523791879@qq.com>
2020-08-26 13:30:48 +0800

    20Aug26 dependencies update

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,14 +2,15 @@
   <div>
     <div class="title">RECOMMEND</div>
     <ul>
-      <li class="item border-bottom" v-for="(item,index) of list" :key="index">
+      <router-link tag="li" class="item border-bottom" v-for="(item,index) of list" 
+                   :key="index" :to="'/detail/' + item.id">
           <img class="item-img" :src='item.imgUrl' />
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">Show Detials</button>
-        </div>
-      </li>
+        </div>]
+      </router-link>
     </ul>
   </div>
 </template>
