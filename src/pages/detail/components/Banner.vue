54a72019134b7586aef8effd04407c4072dfb1d4
CarrolSequal <523791879@qq.com>
2020-08-28 18:20:25 +0800

    20Aug28 Banner Finish

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_r_800x800_087730c7.jpg" />
-    <div class="banner-info">
-      <div class="banner-title">
-        北京市朝阳区东四环小武基北路
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_r_800x800_087730c7.jpg" />
+      <div class="banner-info">
+        <div class="banner-title">
+          北京市朝阳区东四环小武基北路
+          </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe635;</span>
+          39
         </div>
-      <div class="banner-number">
-        <span class="iconfont">&#xe632;</span>
-        39
       </div>
     </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
   </div>
 </template>
 
-<script>
+<script> 
+import CommonGallary from 'common/gallary/Gallary'
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  data () {
+      return {
+        showGallary: false,
+        imgs: ['https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3184901880,3337935316&fm=15&gp=0.jpg', 
+        'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3830243191,3008374358&fm=15&gp=0.jpg']
+      }
+    },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
 }
 </script>
 
@@ -35,14 +57,19 @@ export default {
       bottom: 0
       line-height: .6rem
       color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
       .banner-title
         flex: 1
         font-size: .32rem
         padding:0 .2rem
-        .banner-number
-          padding: 0 .4rem
-          height: .4rem
-          line-height: .4rem 
-          border-radius: .2rem
-          background: rgba(0, 0, 0, .8)
+      .banner-number
+        padding: 0 .4rem
+        height: .32rem
+        line-height: .32rem 
+        margin-top: .14rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
 </style>
