2f21a0ba5d6c578e4f5efe89ea696c1fe813a105
CarrolSequal <523791879@qq.com>
2020-08-28 21:24:32 +0800

    20Aug28b Detail-header Deactivated fuction Update

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -24,7 +24,7 @@ export default {
     }
   },
   methods: {
-    handleScroll () { 
+    handleScroll () {
       const top = document.documentElement.scrollTop
       if (top > 60) {
         let opacity = top / 140
@@ -38,6 +38,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
